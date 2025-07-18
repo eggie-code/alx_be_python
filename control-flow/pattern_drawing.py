157bf965371298b7addf7e3671769be2e2ef3f74
Abdulhabib junis <fatabdi1234@gmail.com>
2025-07-18 15:12:42 +0300

    pattern_drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+# pattern
+# move to the next row after pattern
+
+size = int(input("Enter the size of the pattern: "))
+
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
+    print()
+    row += 1
