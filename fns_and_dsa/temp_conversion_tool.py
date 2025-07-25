69a2a1bf6938e62c7bb98ca795d3b0e5cd645e34
Abdulhabib junis <fatabdi1234@gmail.com>
2025-07-25 11:35:17 +0300

    temp_conversion_tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,7 +5,7 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius using global factor."""
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR) - 32
 
 
 def convert_to_fahrenheit(celsius):
