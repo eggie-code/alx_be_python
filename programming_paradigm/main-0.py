6725d3857fe92838a2f3d28dff9a9dc5127e96cd
Abdulhabib junis <fatabdi1234@gmail.com>
2025-07-31 11:50:07 +0300

    bank account management system

--- programming_paradigm/main-0.py
+++ programming_paradigm/main-0.py
@@ -23,7 +23,7 @@ def main():
             print("Insufficient funds.")
     elif command == "display":
         account.display_balance()
-        print(f"Current Balance: ${self.__account_balance:}")
+        print(f"Current Balance: ${initial_balance:}")
 
     else:
         print("Invalid command.")
