e9e562956508d96025e1b30e08679aa0087c7e86
Abdulhabib junis <fatabdi1234@gmail.com>
2025-08-01 15:40:47 +0300

    library_management

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,14 +4,14 @@ class Book:
         self.author = author
         self._is_checked_out = False
 
-    def Check_out_Book(self):
+    def check_out(self):
         """Check out the book."""
         if not self._is_checked_out:
             self._is_checked_out = True
             return True
         return False
 
-    def Return_Book(self):
+    def return_book(self):
         """Return the book."""
         if self._is_checked_out:
             self._is_checked_out = False
@@ -27,16 +27,15 @@ class Library:
         """Add a book to the library."""
         self.books.append(book)
 
-    def remove_Book(self, book):
-        """Remove a book from the library."""
-        if book in self.books:
-            self.books.remove(book)
-            return True
-        return False
-
-    def find_Book(self, title):
-        """Find a book by title."""
-        for book in self.books:
+    def check_out_book(self, title):
+        """Check out a book by title."""
+        for book in self._books:
             if book.title == title:
-                return book
-        return None
+                book.check_out()
+                print(f"Checked out '{book.title}'.")
+                return True
+            else:
+                print(F"'{book.titke}' is already checked out.")
+                return False
+    print(f"Book title '{title}' not found ")
+    return False
