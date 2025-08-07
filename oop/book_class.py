babdca25a0c66cf95834645a84c6daae1c3d8ba5
Abdulhabib junis <fatabdi1234@gmail.com>
2025-08-07 12:24:16 +0300

    book_class

---
+++ oop/book_class.py
@@ -0,0 +1,14 @@
+class Book:  # creatingb an instance of book
+    def_init_(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+
+    def __del__(self):  # delete a book instance
+        print(f"Deleting {self.title}")
+
+    def __str__(self):
+        return f"{self.title} by {self.author}, published in {self.year}"
+
+    def __repr__(self):
+        return f"Book('{self.title}', '{self.author}', {self.year})"
